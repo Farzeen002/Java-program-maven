844d7c28b4797f06428a838b4e432037f8c68ff4
Farzeen Ahmed <farzeenahmed2000@gmail.com>
2023-11-03 19:33:43 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        
+        String str = "farzeen";
+
+        List<Character> list = new ArrayList<>();
+
+            
+
+        for(char ch : str.toCharArray()){
+
+           list.add(ch);
+
+        }
+
+        for(char c : list){
+            System.out.println(c);
+        }
+
+        
+
+    }
+}
