4d3bd3f4810a20395139422388028b571dd79ec0
Farzeen Ahmed <farzeenahmed2000@gmail.com>
2023-11-03 20:19:31 +0530

    new version

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    
+    public Student(){
+        System.out.println("Creating Student Object");
+    }
+
+    public void sayHello(){
+        System.err.println("My name is Farzeen");
+    }
+
+}
